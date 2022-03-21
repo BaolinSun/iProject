56af40bd3be7a89d8195eef0589a27fee194a948
sunbaolin <baolin.sun@mail.sdu.edu.cn>
2022-03-21 22:27:36 +0800

    add run_eval_mask function

--- run_eval_mask.py
+++ run_eval_mask.py
@@ -1,3 +1,12 @@
-from utils import eval_miou
+import argparse
 
-eval_miou('data/background/valid/pha', 'results')
+from utils import run_eval_miou
+
+parser = argparse.ArgumentParser()
+parser.add_argument('gt_source', type=str)
+parser.add_argument('mask_output', type=str)
+args = parser.parse_args()
+
+run_eval_miou(args.gt_source, args.mask_output)
+
+print('success...')
